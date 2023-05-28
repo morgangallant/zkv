ba3e4565c01b11185380f2f3a740679416624867
Morgan Gallant <morgan@morgangallant.com>
2023-05-28 15:25:46 -0700

    much cleaner zig code
    Signed-off-by: Morgan Gallant <morgan@morgangallant.com>

--- src/main.zig
+++ src/main.zig
@@ -2,38 +2,61 @@ const std = @import("std");
 const testing = std.testing;
 const ArrayList = std.ArrayList;
 
-fn putVarint32(dst: *ArrayList(u8), v: u32) !void {
+fn encodeVarint32(buf: []u8, val: u32) usize {
     const B: u32 = 128;
-    if (v < (1 << 7)) {
-        try dst.append(@truncate(u8, v));
-    } else if (v < (1 << 14)) {
-        try dst.append(@truncate(u8, v | B));
-        try dst.append(@truncate(u8, v >> 7));
-    } else if (v < (1 << 21)) {
-        try dst.append(@truncate(u8, v | B));
-        try dst.append(@truncate(u8, (v >> 7) | B));
-        try dst.append(@truncate(u8, v >> 14));
-    } else if (v < (1 << 28)) {
-        try dst.append(@truncate(u8, v | B));
-        try dst.append(@truncate(u8, (v >> 7) | B));
-        try dst.append(@truncate(u8, (v >> 14) | B));
-        try dst.append(@truncate(u8, v >> 21));
+    if (val < (1 << 7)) {
+        buf[0] = @truncate(u8, val);
+        return 1;
+    } else if (val < (1 << 14)) {
+        buf[0] = @truncate(u8, val | B);
+        buf[1] = @truncate(u8, val >> 7);
+        return 2;
+    } else if (val < (1 << 21)) {
+        buf[0] = @truncate(u8, val | B);
+        buf[1] = @truncate(u8, (val >> 7) | B);
+        buf[2] = @truncate(u8, val >> 14);
+        return 3;
+    } else if (val < (1 << 28)) {
+        buf[0] = @truncate(u8, val | B);
+        buf[1] = @truncate(u8, (val >> 7) | B);
+        buf[2] = @truncate(u8, (val >> 14) | B);
+        buf[3] = @truncate(u8, val >> 21);
+        return 4;
     } else {
-        try dst.append(@truncate(u8, v | B));
-        try dst.append(@truncate(u8, (v >> 7) | B));
-        try dst.append(@truncate(u8, (v >> 14) | B));
-        try dst.append(@truncate(u8, (v >> 21) | B));
-        try dst.append(@truncate(u8, v >> 28));
+        buf[0] = @truncate(u8, val | B);
+        buf[1] = @truncate(u8, (val >> 7) | B);
+        buf[2] = @truncate(u8, (val >> 14) | B);
+        buf[3] = @truncate(u8, (val >> 21) | B);
+        buf[4] = @truncate(u8, val >> 28);
+        return 5;
     }
 }
 
-fn putFixed32(dst: *ArrayList(u8), v: u32) !void {
-    var buf: [@sizeOf(u32)]u8 = undefined;
-    buf[0] = @truncate(u8, v);
-    buf[1] = @truncate(u8, v >> 8);
-    buf[2] = @truncate(u8, v >> 16);
-    buf[3] = @truncate(u8, v >> 24);
-    try dst.appendSlice(&buf);
+fn encodeFixed32(buf: []u8, val: u32) void {
+    buf[0] = @truncate(u8, val);
+    buf[1] = @truncate(u8, val >> 8);
+    buf[2] = @truncate(u8, val >> 16);
+    buf[3] = @truncate(u8, val >> 24);
+}
+
+const varint32 = struct {
+    val: u32,
+    bytesRead: usize,
+};
+
+fn decodeVarint32(buf: []const u8) !varint32 {
+    var result: u32 = 0;
+    var shift: u32 = 0;
+    var i: usize = 0;
+    while (i < 5) : (i += 1) {
+        const b = buf[i];
+        result |= @truncate(u32, std.math.shl(u32, b & 0x7f, shift));
+        if (b < 0x80) {
+            return varint32{ .val = result, .bytesRead = i + 1 };
+        }
+        shift += 7;
+    }
+    return error.InvalidVarint32;
 }
 
 fn NewBlockBuilder(
@@ -49,13 +72,24 @@ fn NewBlockBuilder(
         last_key: ArrayList(u8),
 
         pub fn init(allocator: std.mem.Allocator) !BlockBuilderType {
+            var restarts = ArrayList(u32).init(allocator);
+            try restarts.append(0); // first restart point is at offset 0
             return BlockBuilderType{
                 .block = ArrayList(u8).init(allocator),
-                .restarts = ArrayList(u32).init(allocator),
+                .restarts = restarts,
                 .restart_count = 0,
                 .finished = false,
                 .last_key = ArrayList(u8).init(allocator),
             };
+        }
+
+        pub fn reset(self: *BlockBuilderType) !void {
+            self.block.clearRetainingCapacity();
+            self.restarts.clearRetainingCapacity();
+            try self.restarts.append(0); // first restart point is at offset 0
+            self.restart_count = 0;
+            self.finished = false;
+            self.last_key.clearRetainingCapacity();
         }
 
         pub fn deinit(self: *BlockBuilderType) void {
@@ -88,9 +122,9 @@ fn NewBlockBuilder(
             const non_shared = @intCast(u32, key.len) - shared;
 
             // <shared><non_shared><value_size>
-            try putVarint32(&self.block, shared);
-            try putVarint32(&self.block, non_shared);
-            try putVarint32(&self.block, @intCast(u32, value.len));
+            try self.emitVarint32(shared);
+            try self.emitVarint32(non_shared);
+            try self.emitVarint32(@intCast(u32, value.len));
 
             // string delta of key + value
             try self.block.appendSlice(key[shared..]);
@@ -110,11 +144,23 @@ fn NewBlockBuilder(
         pub fn finish(self: *BlockBuilderType) ![]const u8 {
             // Write restart array offsets
             for (self.restarts.items) |offset| {
-                try putFixed32(&self.block, offset);
+                try self.emitFixed32(offset);
             }
-            try putFixed32(&self.block, @intCast(u32, self.restarts.items.len));
+            try self.emitFixed32(@intCast(u32, self.restarts.items.len));
             self.finished = true;
             return self.block.toOwnedSlice(); // Caller owns the slice
+        }
+
+        fn emitVarint32(self: *BlockBuilderType, val: u32) !void {
+            var buf: [5]u8 = undefined;
+            const used = encodeVarint32(&buf, val);
+            try self.block.appendSlice(buf[0..used]);
+        }
+
+        fn emitFixed32(self: *BlockBuilderType, val: u32) !void {
+            var buf: [4]u8 = undefined;
+            encodeFixed32(&buf, val);
+            try self.block.appendSlice(&buf);
         }
     };
 }
@@ -125,14 +171,13 @@ test "block builder" {
     var builder = try NewBlockBuilder(16).init(allocator);
     defer builder.deinit();
 
-    // make sure to add newline to the end of the string
     try builder.add("a", "val1");
     try builder.add("ab", "val2");
     try builder.add("abc", "val3");
     try builder.add("bcd", "val4");
 
-    const block = try builder.finish(); // we're responsible for freeing the block
-    // print block
-    std.debug.print("block: {s}\n", .{std.fmt.fmtSliceHexLower(block)});
+    const block = try builder.finish(); // once finished, we own the slice
     defer allocator.free(block);
+
+    std.debug.print("block: {s}\n", .{std.fmt.fmtSliceHexLower(block)});
 }
